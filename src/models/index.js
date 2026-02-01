7929f511462d42cf374c95ed64ceee7587b93914
NandakishorP <nandakishor22770@gmail.com>
2026-02-01 15:14:10 +0530

    feat(models): add model barrel exports

---
+++ src/models/index.js
@@ -0,0 +1,27 @@
+const User = require('./User');
+const Menu = require('./Menu');
+const MenuItem = require('./MenuItem');
+const Slot = require('./Slot');
+const Booking = require('./Booking');
+const SystemSetting = require('./SystemSetting');
+const Notification = require('./Notification');
+const AuditLog = require('./AuditLog');
+const Holiday = require('./Holiday');
+const WasteReport = require('./WasteReport');
+const Forecast = require('./Forecast');
+const Canteen = require('./Canteen');
+
+module.exports = {
+  User,
+  Menu,
+  MenuItem,
+  Slot,
+  Booking,
+  SystemSetting,
+  Notification,
+  AuditLog,
+  Holiday,
+  WasteReport,
+  Forecast,
+  Canteen,
+};
